0be10a8afba3c755b8832d0e32c58791907c5909
사삭이 <85689028+sasak2@users.noreply.github.com>
2024-02-02 13:00:22 +0900

    Update 선분교차판정.rs

--- 선분교차판정.rs
+++ 선분교차판정.rs
@@ -9,37 +9,25 @@ fn sub(a: P, b: P) -> P { [a[0] - b[0], a[1] - b[1]] }
 fn dot(a: P, b: P) -> I { a[0] * b[0] + a[1] * b[1] }
 fn cross(a: P, b: P) -> I { a[0] * b[1] - a[1] * b[0] }
 fn ccw(a: P, b: P, c: P) -> I { cross(sub(b, a), sub(c, b)) }
-//https://bamgoesn.github.io/rust-ps-md/geometry/2dgeometrybase.html 밤고수 템플릿 뺏어쓰기 hehe
-//0,0 처리 못하니까 나중에 알아서 잘 처리해라 미래의 나
-fn is_GyoCha(l1:L,l2:L)->bool {
-//https://killerwhale0917.tistory.com/6
-    let mut p1=l1[0];
-    let mut p2=l1[1];
-    let mut p3=l2[0];
-    let mut p4=l2[1];
-    let temp:i64=0;
+//https://bamgoesn.github.io/rust-ps-md/geometry/2dgeometrybase.html 밤고수 템플릿 뺏어쓰기 heheThe code below only checks if two lines meet, without calculating where they do.
 
-    let p1p2 = ccw(p1, p2, p3) * ccw(p1, p2, p4);
-    let p3p4 = ccw(p3, p4, p1) * ccw(p3, p4, p2); 
-    
-
-    if p1==p3||p2==p4||p1==p4||p2==p3 {
-        return true;
-    }
-    if p1p2 == 0 && p3p4 == 0 {
-    	if p1 > p2 { 
-            let temp=p1;
-             p1=p2;
-             p2=temp;
+//https://bamgoesn.github.io/rust-ps-md/geometry/line_intersection.html
+/// Checks if line segments `p` and `q` intersect.
+/// Returns `true` if they intersect at any point, `false` otherwise.
+fn meets(p: L, q: L) -> bool {
+    let u = cross(sub(p[1], p[0]), sub(q[1], q[0]));
+    let sn = cross(sub(q[0], p[0]), sub(q[1], q[0]));
+    let tn = cross(sub(q[0], p[0]), sub(p[1], p[0]));
+    if u != 0 {
+        let int = if u >= 0 { 0..=u } else { u..=0 };
+        int.contains(&sn) && int.contains(&tn)
+    } else {
+        if sn != 0 || tn != 0 {
+            return false;
         }
-        if p3 > p4 {
-            let temp=p3;
-             p3=p4;
-             p4=temp;
-        }
-        
-        return p3 <= p2 && p1 <= p4;
+        let (a0, a1) = (p[0].min(p[1]), p[0].max(p[1]));
+        let (b0, b1) = (q[0].min(q[1]), q[0].max(q[1]));
+        let (l, r) = (a0.max(b0), a1.min(b1));
+        l <= r
     }
-    
-    return p1p2 <= 0 && p3p4 <= 0;
 }
